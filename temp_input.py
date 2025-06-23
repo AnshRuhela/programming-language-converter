80870b1747a135c3091cb1b7ac77ddebbc84dd3d
ansh <rohillaansh5@gmail.com>
2025-06-23 12:14:35 +0530

    remove_unused_files

--- temp_input.py
+++ temp_input.py
@@ -1,7 +1,5 @@
 import math
 
-def sum(a,b):
-    return a+b
 
 
 def fact(n):
@@ -9,30 +7,81 @@ def fact(n):
         return 1
     return n * fact(n - 1)
 
-num1=int(input("enter num"))
-num2=int(input("enter num:"))
-
-sum1=sum(num1,num2)
-print(sum1)
-
-math.sqrt(40)
-d=math.ceil(34)
-math.floor(34)
-
-min(4,3)
-
-arr=[1,2,3,4]
-le=[1.2,3.4]
-
-huur=["34","44"]
-
-rr=["d"]
-
 def factorial(n):
     if n == 0 or n == 1:
         return 1
     else:
         return n * factorial(n - 1)
 
-# Example usage
-print(factorial(5))
+def func(a,b,c):
+    print("helo")
+
+def func2():
+    for i in range(30):
+        print("hl")
+
+
+def func3():
+    print("hlo")
+    for i in range(300):
+        print("hl")
+    print("4")
+
+def func4():
+    print("this is fun")
+    return 1
+
+
+print("hlo")
+name = input("enter name: ")
+age = int(input("enter age: "))
+a = 30
+b = 30
+cc = 3.4
+rr = 3
+nn = float(input("enter number: "))
+print(nn)
+
+x = 0
+while x < 3:
+    print(x)
+    x += 1
+
+if 4:
+    print("two ram ram ram ram")
+
+for i in range(30):
+    print("tone")
+
+for i in range(43):
+    if 3: 
+        print("hle")
+    elif 34:
+        print("ee")
+        for j in range(30):
+            print('d')
+
+for i in range(43): # this is comm
+    if 3:
+        print("hle")
+    elif 34:
+        print("ee")
+    else:
+        print("dfdgj")
+
+func2()
+func3()
+math.ceil(3.4)
+math.floor(44.4)
+math.pow(3, 4)
+RAM=0
+RAM=fact(4)
+
+
+arr = [1, 2, 3, 4, 5]
+
+arr2=["df"]
+
+arr3=[3.4,5.4]
+
+x=min(9,45)
